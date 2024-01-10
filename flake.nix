545d1d752821e22a631be71de4d09c340bcab85d
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-01-10 13:28:21 +0100

    add aarch64-linux, aarch64-darwin, x86_64-darwin

--- flake.nix
+++ flake.nix
@@ -5,25 +5,51 @@
   };
   outputs = { self, nixpkgs, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      risc0pkgs = pkgs.recurseIntoAttrs (pkgs.callPackage ./pkgs { });
-      lib = pkgs.recurseIntoAttrs (pkgs.callPackage ./lib { pkgs = pkgs // risc0pkgs; });
+      eachSystem = systems: f:
+        let
+          # Merge together the outputs for all systems.
+          op = attrs: system:
+            let
+              ret = f system;
+              op = attrs: key: attrs //
+                {
+                  ${key} = (attrs.${key} or { })
+                    // { ${system} = ret.${key}; };
+                }
+              ;
+            in
+            builtins.foldl' op attrs (builtins.attrNames ret);
+        in
+        builtins.foldl' op { } systems;
+
+      eachDefaultSystem = eachSystem [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
     in
     {
-      lib.${system} = lib;
-      packages.${system} = { inherit (risc0pkgs) r0vm rustc0; };
-      formatter.${system} = pkgs.nixpkgs-fmt;
-
-      checks.${system}.format = pkgs.runCommand "format-check" { buildInputs = [ pkgs.nixpkgs-fmt ]; } ''
-        set -euo pipefail
-        cd ${self}
-        nixpkgs-fmt --check .
-        touch $out
-      '';
       templates.default = {
         path = ./templates/default;
         description = "risc0 project template";
       };
-    };
+    }
+    // eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        risc0pkgs = pkgs.recurseIntoAttrs (pkgs.callPackage ./pkgs { });
+        lib = pkgs.recurseIntoAttrs (pkgs.callPackage ./lib { pkgs = pkgs // risc0pkgs; });
+      in
+      {
+        inherit lib;
+        packages = { inherit (risc0pkgs) r0vm rustc0; };
+        formatter = pkgs.nixpkgs-fmt;
+
+        checks.format = pkgs.runCommand "format-check" { buildInputs = [ pkgs.nixpkgs-fmt ]; } ''
+          set -euo pipefail
+          cd ${self}
+          nixpkgs-fmt --check .
+          touch $out
+        '';
+      });
 }
